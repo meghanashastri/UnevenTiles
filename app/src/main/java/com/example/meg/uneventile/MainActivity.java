fbd9fa4c913b6c5fb8425a94ad736f5f339699c2
Meghana Shastri <meghana@wobe.com>
2017-12-05 10:05:32 +0530

    Keen data seen on Dashboard

--- app/src/main/java/com/example/meg/uneventile/MainActivity.java
+++ app/src/main/java/com/example/meg/uneventile/MainActivity.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import io.keen.client.android.AndroidKeenClientBuilder;
 import io.keen.client.java.KeenClient;
+import io.keen.client.java.KeenLogging;
 import io.keen.client.java.KeenProject;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
         KeenClient client = new AndroidKeenClientBuilder(this).build();
         KeenClient.initialize(client);
+        KeenLogging.enableLogging();
+        client.setDebugMode(true);
 
         // This is your actual Project ID and Write Key
         KeenProject project = new KeenProject("5a1fd52fc9e77c0001ab61d2", "BA7A6BF5E961C85E635635C43966648A18D0A5E4FE0CF222FEDEF1DAEE6A078046E8DA79FE32B54A0A964C5CC11A0D5EDA5B93FCDDC9E60960B8436318EE87753CCBEF15FFA57F06B8D3B49C93475E099687243AA705F9ACA21976A95A3626CB", null);
@@ -29,12 +32,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void track() {
-        // Create an event to upload to Keen.
-        Map<String, Object> event = new HashMap<String, Object>();
-        event.put("item", "AppOpen");
 
-        KeenClient.client().addEvent("appOpen", event);
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    // Create an event to upload to Keen.
+                    Map<String, Object> event = new HashMap<String, Object>();
+                    event.put("item", "AppOpen");
+
+                    KeenClient.client().addEvent("appOpen", event);
+                } catch (Exception e) {
+                    // do nothing
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        thread.start();
     }
+
 
 
 }
